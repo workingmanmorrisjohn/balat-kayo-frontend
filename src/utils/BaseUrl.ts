3f4d9574bf450dd2707a64d657c2a427ad7118d1
Morris Montemayor <morris.montemayor@azcalun.com>
2025-05-15 16:11:28 +0800

    url

--- src/utils/BaseUrl.ts
+++ src/utils/BaseUrl.ts
@@ -1,7 +1,7 @@
 export const BASE_URL = 'https://api.datejournals.com';
-// export const WEBSOCKET_URL = "ws://balatkayobackend.datejournals.com"
-// export const ROOM_BASE_URL = "http://balatkayobackend.datejournals.com"
+export const WEBSOCKET_URL = "wss://balatkayobackend.datejournals.com"
+export const ROOM_BASE_URL = "https://balatkayobackend.datejournals.com"
 
 // For Testing
-export const WEBSOCKET_URL = "ws://localhost:8000"
-export const ROOM_BASE_URL = "http://localhost:8000"
+// export const WEBSOCKET_URL = "ws://localhost:8000"
+// export const ROOM_BASE_URL = "http://localhost:8000"
